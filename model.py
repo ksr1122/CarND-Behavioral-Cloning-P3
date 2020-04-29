8f590adfabc80f384c22ea859c72cfa3d349c2b2
ravi_ks <ksr1122@gmail.com>
2020-04-29 08:31:21 +0200

    feat: implement training with keras models
     * setup generator callback function
     * setup a cnn architecture to fit samples

--- model.py
+++ model.py
@@ -58,3 +58,49 @@ def generator(samples, batch_size=32):
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
+
+# Set our batch size
+batch_size=128
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
+
+y_size, x_size = 160, 320
+crop_top, crop_bottom = 70, 25
+row, col, ch = (y_size - crop_top - crop_bottom), x_size, 3  # Trimmed image format
+
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, Cropping2D, Dense, Flatten, Lambda
+
+def train():
+    model = Sequential()
+
+    # Cropping
+    model.add(Cropping2D(cropping=((crop_top, crop_bottom), (0,0)), input_shape=(y_size, x_size, ch)))
+    # Normalize & Mean centere
+    model.add(Lambda(lambda x: x/127.5 - 1.0, input_shape=(row, col, ch), output_shape=(row, col, ch)))
+
+    # NVIDIA's arch
+    model.add(Conv2D(24,(5,5),activation='relu',strides=(2,2)))
+    model.add(Conv2D(36,(5,5),activation='relu',strides=(2,2)))
+    model.add(Conv2D(48,(5,5),activation='relu',strides=(2,2)))
+    model.add(Conv2D(64,(3,3),activation='relu'))
+    model.add(Conv2D(64,(3,3),activation='relu'))
+
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+
+    history_object = model.fit_generator(train_generator, \
+                                         steps_per_epoch=ceil(len(train_samples)/batch_size), \
+                                         validation_data=validation_generator, \
+                                         validation_steps=ceil(len(validation_samples)/batch_size), \
+                                         epochs=5, verbose=1)
+
+    model.save('model.h5')
+    return history_object
